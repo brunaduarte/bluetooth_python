5ef25ec2d0f335405ae94c66618cb2ed29e9e727
Bruna Duarte <brubruninhas@hotmail.com>
2016-03-27 18:18:31 +0100

    Update espeak.py

--- espeak.py
+++ espeak.py
@@ -1,9 +1,9 @@
-import os  
+import os
 
 name1 = "Hello, Hello Bruna"
 name2 = "Hello, Hello Button"
 
 def say(name):
- os.system('espeak -ven+f2 -k5 -s150 "{}"'.format(name))
- 
-say(name2)
+    os.system('espeak -ven+f2 -k5 -s150 "{}"'.format(name))
+
+    say(name2)
